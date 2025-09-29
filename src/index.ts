4b3be56fbbc0c2bb71729efffcdd34d32f6690ba
Noman Ahmed <noman.ahmed1408@gmail.com>
2025-09-29 18:12:05 +0500

    first commit / typeorm setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import express from "express";
+import { AppDataSource } from "./config/data-source.ts";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(PORT, () => {
+      console.log("Server is running on http://localhost:" + PORT);
+    });
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
